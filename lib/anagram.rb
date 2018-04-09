552689a785ccbe694f2aa60744943ca87156b203
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-04-09 22:47:06 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
   end
   
   def match(array)
-    arr.keep_if { |str| name.split(" ").sort == str.split(" ").sort}
+    array.keep_if { |str| name.split(" ").sort == str.split(" ").sort}
   end
   
 end
